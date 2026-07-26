9246911452eee43e582daa375cba2c2009f71558
jatin300107 <jatin30jan2007@gmail.com>
2026-07-26 23:47:43 +0530

    added frontend CORS permission

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,12 +5,16 @@ from fastapi.responses import FileResponse
 from backend.auth.auth_routes import router
 from backend.farmer.endpoints import farmer, expert
 from backend.ai_pipeline.api_endpoint import ai_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
-
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -26,3 +30,4 @@ app.include_router(router)
 app.include_router(farmer)
 app.include_router(expert)
 app.include_router(ai_router)
+
